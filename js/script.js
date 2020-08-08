a0a913cf2e7a441e4be459276355fd0d1eaeaad3
v.ITalik <vitalik.gricenko77@gmail.com>
2020-08-08 18:03:12 +0300

    First version programm

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+// Условия if switch
+
+// let num = 50;
+// if (num < 49) {
+//   console.log('не');
+// } else if (num > 100) {
+//   console.log('оно больше ста')
+// } else {
+//   console.log('тут 50')
+// };
+
+// (num == 50) ? console.log('Здесь 50') : console.log('Не 50');
+
+
+
+// switch (num) {
+//   case num < 49:
+//     console.log('49');
+//     break;
+//   case 50:
+//     console.log('50');
+//     break;
+//   case num > 55:
+//     console.log('55');
+//     break;
+//   default:
+//     console.log('Дурак?');
+//     break;
+// }
+
+
+// let scriptJ = prompt('какое оффициальное название JS?', '');
+
+// if (scriptJ == 'ECMAScript') {
+//   alert('О да! Молодец, ты знаешь!');
+// } else {
+//   alert('Ты не знаешь ECMAScript?');
+// }
+
+
+
+
+// alert('Hi');
+
+// const result = confirm('Ты кто?');
+
+// console.log(result);
+
+// const answer = prompt('вам есть 18?', '');
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+
+const numberOfFilms = prompt('')
